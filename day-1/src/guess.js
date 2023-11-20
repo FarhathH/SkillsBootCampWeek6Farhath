f6abe66eab10457315bfadbd44c797379d73a151
FarhathH <farhathh0@gmail.com>
2023-11-20 13:51:38 +0000

    Added a another box

--- day-1/src/guess.js
+++ day-1/src/guess.js
@@ -6,29 +6,37 @@ import {useState} from 'react';
 
 function RandomNum(){
    
+    // useState variable to use for checking condition
     const[guess, setGuess] = useState("")
     const[answer, setAnswer] = useState(5)
     const [message, setMessage] = useState("Guess the number between 1 and 10")  
 
+    // This should change the message based on input from user.
     function MakeGuess(){
-        if(setGuess<answer){
-            setMessage('Too low')
+        if(guess<answer){
+            setMessage("Too low")
         }
-        else if(setGuess>answer){
-            setMessage('Too high')
+        else if(guess>answer){
+            setMessage("Too high")
+        }else{
+            setMessage("You got it!")
         }
    }
    
-   
+//    This should be displayed on the interface
     return(
     <>
-        <h2>{message}</h2>
-        {typeof guess}
-        <div className = "layout">
-            <input type = "number" className = "input-guess" onChange={(event)=>{setGuess(parseInt(event.target.value))}}/>     
-            <button onClick = {MakeGuess} className = "guess-button">
-                <strong>GUESS</strong>
-            </button>
+        <div className = "box">
+            <h2>{message}</h2>
+            {/* {typeof guess} */}
+            <div className = "layout">
+                {/* onChange function takes input as setGuess. */}
+                <input type = "number" className = "input-guess" onChange={(event)=>{setGuess(parseInt(event.target.value))}}/> 
+                {/*Checks for the correct answer when the button is clicked*/}    
+                <button onClick = {MakeGuess} className = "guess-button">
+                    <strong>GUESS</strong>
+                </button>
+            </div>
         </div>
         
     </>
