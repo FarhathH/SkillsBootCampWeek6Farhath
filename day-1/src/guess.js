43a2631df632f50d4b044fd0dfcbb219eb590a84
FarhathH <farhathh0@gmail.com>
2023-11-20 12:44:39 +0000

    Trying to fix the function for triggering a message change response.

--- day-1/src/guess.js
+++ day-1/src/guess.js
@@ -6,21 +6,35 @@ import {useState} from 'react';
 
 function RandomNum(){
    
-    const[guess, setGuess] = useState("") 
+    const[guess, setGuess] = useState("")
+    const[answer, setAnswer] = useState(5)
+    const [message, setMessage] = useState("Guess the number between 1 and 10")  
+
+    function MakeGuess(){
+        if(setGuess<answer){
+            setMessage('Too low')
+        }
+        else if(setGuess>answer){
+            setMessage('Too high')
+        }
+   }
+   
    
     return(
     <>
-        <h2>Guess the number:</h2>
+        <h2>{message}</h2>
         {typeof guess}
         <div className = "layout">
             <input type = "number" className = "input-guess" onChange={(event)=>{setGuess(parseInt(event.target.value))}}/>     
-            <button className = "guess-button">
+            <button onClick = {MakeGuess} className = "guess-button">
                 <strong>GUESS</strong>
             </button>
         </div>
         
     </>
    );
+
+
 
 }
 
