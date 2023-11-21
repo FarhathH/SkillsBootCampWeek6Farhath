91be0c2fb45973595074021358a29e73f064b8a1
FarhathH <farhathh0@gmail.com>
2023-11-21 13:48:36 +0000

    new images

--- day-2/src/App.js
+++ day-2/src/App.js
@@ -1,12 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import User from './user.js'; //this allows the app page to take the function
+import {useState} from 'react';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <User/> {/*rendering the function*/}
+        <User name = {"Penni the Dancing Cholo"} age = {45} profession = {"Streamer"} pic = {'koala.jpg'}/> {/*rendering the function*/}
+        <User name = {"Let me in"} age = {36} profession = {"Animator"} pic = {'sr-pelo-google-form.jpg'}/> {/*rendering the function*/}
+        <User name = {"These crawls they will not crawl"} age = {19} profession = {"Edgelord"} pic = {'crawling.jpg'}/> {/*rendering the function*/}
       </header>
     </div>
   );
--- day-2/src/user.js
+++ day-2/src/user.js
@@ -1,15 +1,27 @@
 import './socialMedia.css'; //importing the stylesheet I made
+import {useState} from 'react';
+
+
+
+
+
 
 //Made functional component to displaying intro
-function User(){
+function User(props){
+    const[name, setName] = useState(props.name);
+    const[age, setAge] = useState(props.age);
+    const[profession, setProfession] = useState(props.profession);
+    const[pic, setPic] = useState(props.pic);
+  
+    
     return( //html below displays info about user
         <div className = "page">
-            <div className = "img"><img src = "koala.jpg" className = "img2"></img></div>
+            <div className = "img"><img src = {pic} className = "img2"></img></div>
             
             <div className = "info">
-                <p className = "chunk2">Name: Pepito</p>
-                <p className = "chunk">Age: 35</p>
-                <p className = "chunk">Profession: Gaming Streamer</p>
+                <p className = "chunk2">Name:{name} </p>
+                <p className = "chunk">Age: {age} </p>
+                <p className = "chunk">Profession: {profession} </p>
             </div>
         </div>
     );
