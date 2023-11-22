d1c011dee054b74026d6ba87933dab5b3e6ce57d
FarhathH <farhathh0@gmail.com>
2023-11-22 14:14:15 +0000

    added functional components

---
+++ day-3-calculator/src/calculator.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import {calculatorDisplay} from './calculatorDisplay';
+import {calculatorButton} from './calculatorButton';
+
+export const Calculator = () => {
+    return(
+        <div>Calculator</div>
+    );
+}
