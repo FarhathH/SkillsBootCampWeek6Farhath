ded56fa27271c4624768d3b73421794b31a0c920
FarhathH <farhathh0@gmail.com>
2023-11-23 09:59:58 +0000

    made react app

---
+++ day-4/src/calculator.js
@@ -0,0 +1,11 @@
+
+//gives us the layout of the calculator.
+function calculator(){
+    return(
+        <div className = "main-body">
+            <div className="display-screen"></div>
+            <div className="operators"></div>
+            <div className="num-btns"></div>
+        </div>
+    );
+}
