1f9b13d6562e9dcf95e7662889089cf2ece73c1d
FarhathH <farhathh0@gmail.com>
2023-11-23 11:21:48 +0000

    fixing the layout

--- day-4/src/calculator.js
+++ day-4/src/calculator.js
@@ -1,19 +1,40 @@
+import './calculator.css'
 
 //gives us the layout of the calculator.
-function Calculator(){
+export default function Calculator(){
     return(
         <div className = "calc-container">
             <div className="calc-space">
                 <div className="calc-display"></div>
             </div>
             <div className="calc-operators">
-
+                <button className='buttons'></button>
+                <button className='buttons'></button>
+                <button className='buttons'></button>
+                <button className='buttons'></button>
             </div>
             <div className="calc-numbers">
-
+                <button className='num-buttons'></button>
+                <button className='num-buttons'></button>
+                <button className='num-buttons'></button>
             </div>
+            <div className="calc-numbers">
+                <button className='num-buttons'></button>
+                <button className='num-buttons'></button>
+                <button className='num-buttons'></button>
+            </div>
+            <div className="calc-numbers">
+                <button className='num-buttons'></button>
+                <button className='num-buttons'></button>
+                <button className='num-buttons'></button>
+            </div>
+            <div className="calc-numbers">
+                <button className='num-buttons'></button>
+                <button className='num-buttons'></button>
+                <button className='num-buttons'></button>
+            </div>
+            
         </div>
     );
 }
 
-export default Calculator;
