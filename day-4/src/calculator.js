7370f1f46b41bc03cb22f89906e41b09c8f73ed5
FarhathH <farhathh0@gmail.com>
2023-11-23 16:30:48 +0000

    adding other functions

--- day-4/src/calculator.js
+++ day-4/src/calculator.js
@@ -4,28 +4,35 @@ import {useState} from 'react'; //allows us to use the useState function
 export default function Calculator(){ 
 
     //arrays for the button text.
-    const[mathSymbols, setMathSymbols] = useState(["+","-","*","/","="]);
+    const[mathSymbols, setMathSymbols] = useState(["+","-","*","/"]);
     const[firstNumbers, setfirstNumbers] = useState(['9','8','7','6','5','4','3','2','1','0']);
     
     //use for input and results
     const[input, setInput] = useState('');
-    const[result, setResult] = useState('');
-
-    function buttonPress(){ //function executes when user clicks the button
-       
+    const[output, setOutput] = useState('');
+    
+   
+    //executes when the characters length is less than 16
+  function handleInput(text){
+    if(output.length < 16){
+        setOutput(output + text)
     }
+  }
+   
 
 
 
     return( //this layout is executed when the function is rendered.
         <div className = "calc-container">
             <div className="calc-space">
-                <div className="calc-display"></div>
+                <div className="calc-display">
+                    <p>{output}</p>
+                </div>
             </div>
             <div className="calc-operators">
                 {mathSymbols.map((math)=>{
                     return( //iterates the operator buttons
-                        <button onClick = {buttonPress} className='buttons' style={{width:"100%", height:"75%"}}>{math}</button>
+                        <button onClick = {()=>{handleInput(math)}}className='buttons' style={{width:"100%", height:"75%"}}>{math}</button>
                     );
                 })}
                
@@ -34,12 +41,17 @@ export default function Calculator(){
             <div className="calc-numbers">
                 {firstNumbers.map((num)=>{
                     return( //iterates the number buttons
-                        <button onClick = {buttonPress} className='num-buttons' style = {{width:"33%",height:"25%"}}>{num}</button>
+                        //onclick parameter
+                        <button onClick = {()=>{handleInput(num)}} className='num-buttons' style = {{width:"33%",height:"25%"}}>{num}</button>
                     );
                 })}
-               
-                   
+                {/*these two buttons will have different onClick functions*/}
+               <button onClick = {()=>{{setOutput('')}}} className='num-buttons' style = {{width:"33%",height:"25%"}}>Clear</button>
+               <button onClick = {()=> {{setOutput(Function('return ' + output)())}}} className='num-buttons' style = {{width:"33%",height:"25%"}}>=</button>   
             </div>
+            
+                
+            
             
         </div>
     );
